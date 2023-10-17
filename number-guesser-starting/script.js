81aec6e0abfea0effa34ea5869034b29666ff222
lyzboy <henrylongbottoms@gmail.com>
2023-10-16 21:02:26 -0600

    Complete number-guesser-starting/script.js
    Complete the challenge for number guesser code.

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -2,5 +2,32 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Generate the target number
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
 
+// Determine the winner of the round
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+  const humanPoints = Math.abs(humanGuess - targetNumber);
+  const computerPoints = Math.abs(computerGuess - targetNumber);
+  return humanPoints <= computerPoints ? true : false;
+};
+
+// update the score based on the winner
+const updateScore = (winner) => {
+  if (winner === "human") humanScore++;
+  else computerScore++;
+};
+
+// advance the round
+const advanceRound = () => {
+  currentRoundNumber++;
+};
+
+// simple test
+let testTarget = generateTarget();
+console.log(`Human #: 8
+Computer #: 3
+Target #: ${testTarget}
+Human Won?: ${compareGuesses(8, 3, testTarget)}`);
